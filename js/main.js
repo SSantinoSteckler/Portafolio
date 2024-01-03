787a2268d16c44faf88a736ba4789d5324834ce1
Lucas Santino Steckler <lucassteckler1@gmail.com>
2024-01-03 14:57:07 -0300

    commit prueba

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const nav = document.querySelector('#nav');
 const abrir = document.querySelector('#abrir');
 const cerrar = document.querySelector('#cerrar');
+const link = document.querySelector('#link');
 
 abrir.addEventListener('click', () => {
   nav.classList.add('visible');
