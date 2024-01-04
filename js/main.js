1086f97ab5d007527deddd30f1c39180b2098fbf
Lucas Santino Steckler <lucassteckler1@gmail.com>
2024-01-04 03:16:20 -0300

    commit animaciones

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
-const nav = document.querySelector('#nav');
-const abrir = document.querySelector('#abrir');
-const cerrar = document.querySelector('#cerrar');
-const link = document.querySelector('#link');
+document.addEventListener('DOMContentLoaded', function () {
+  const nav = document.querySelector('#nav');
+  const abrir = document.querySelector('#abrir');
+  const cerrar = document.querySelector('#cerrar');
+  const links = document.querySelectorAll('.nav a');
 
-abrir.addEventListener('click', () => {
-  nav.classList.add('visible');
-});
+  abrir.addEventListener('click', () => {
+    nav.classList.add('visible');
+  });
 
-cerrar.addEventListener('click', () => {
-  nav.classList.remove('visible');
+  cerrar.addEventListener('click', () => {
+    nav.classList.remove('visible');
+  });
+
+  // Agrega el evento de clic a cada enlace
+  links.forEach((link) => {
+    link.addEventListener('click', () => {
+      nav.classList.remove('visible');
+    });
+  });
 });
